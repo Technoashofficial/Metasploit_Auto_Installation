8c071f39bcffc792bd80dcff118e16ba62269add
Technoashofficial <38762992+Technoashofficial@users.noreply.github.com>
2018-05-02 16:40:53 +0530

    Update Metasploit_Termux_Auto_Install.sh

--- Metasploit_Termux_Auto_Install.sh
+++ Metasploit_Termux_Auto_Install.sh
@@ -1,3 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
+pkg install python
+pkg install python2
+pkg install curl
+pkg install wget
+pkg install perl
+pkg install ruby
+pkg install php
 wgethttps://Auxilus.github.io/metasploit.sh
 bash metasploit.sh
